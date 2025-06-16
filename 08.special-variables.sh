d40edebf1733a0ed801106b8cc9cf6da26e12952
Naveen-Avula <avulanaveen437@gmail.com>
2025-06-16 12:42:37 +0530

    msg

--- 08.special-variables.sh
+++ 08.special-variables.sh
@@ -2,6 +2,12 @@
 echo "All variables :$@"
 echo "No of variables :$#"
 echo "Script name:$0"
-echo "All variables:$*"
-
+echo "All variables:$*" # another way
+echo "current working directory:$PWD"
+echo "current user name:$USER"
+echo "current shell name:$SHELL"
+echo "current date and time:$DATE"
+echo "home directory:$HOME"
+echo "Process ID of current shell:$$"
+echo "PID of last background process:$!"
 

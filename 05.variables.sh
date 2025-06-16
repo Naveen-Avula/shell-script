62936c4b3bc3d49de07f28dc16d3908cf5fc07a7
Naveen-Avula <avulanaveen437@gmail.com>
2025-06-16 10:59:43 +0530

    add-all

--- 05.variables.sh
+++ 05.variables.sh
@@ -4,3 +4,4 @@ read -s USERNAME #here username is a variable
 echo "pls enter the password::"
 read -s PASSWORD #here password is a variable
 echo "Username is:  $USERNAME, Password is:  $PASSWORD"
+echo "checking the username and password"

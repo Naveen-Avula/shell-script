cbd3363e6d01df6060d7004e404d98278d7d6d89
Naveen-Avula <avulanaveen437@gmail.com>
2025-06-16 11:08:50 +0530

    add-all

---
+++ 04.variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "$1:Hi $2, how are u?"
+echo "$2: I am fine $1, how about you?"
+echo "$1: I am fine too, thanks for asking"  
+echo "$2: I am learning shell scripting"
+echo "$1: I am learning shell scripting too"
+echo "$2: I am learning shell scripting too....!@"

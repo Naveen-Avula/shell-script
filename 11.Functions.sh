90745b3cf7d7e85571bf3a2efebfcb80aab852bf
Naveen-Avula <avulanaveen437@gmail.com>
2025-06-19 19:47:29 +0530

    msg

--- 11.Functions.sh
+++ 11.Functions.sh
@@ -10,18 +10,15 @@ echo "you are the root user"
 fi
 
 VALIDATE(){
-    if [$1 -ne 0]
+    if [ $1 -ne 0 ]
     then 
-    echo "$2...Failure"
-    exit 1
+      echo "$2...Failure"
+      exit 1
     else
-    echo"$2...Success"
+       echo"$2...Success"
     fi
 
 }
-
-
-
 
 dnf install mysql -y
 VALIDATE $? "Installing my sql"

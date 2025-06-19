d5287c280c1a8b7dac2985f0706929e45605cbf9
Naveen-Avula <avulanaveen437@gmail.com>
2025-06-19 19:52:51 +0530

    msg

--- 11.Functions.sh
+++ 11.Functions.sh
@@ -12,10 +12,10 @@ fi
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-      echo "$2...Failure"
-      exit 1
+        echo "$2...Failure"
+         exit 1
     else
-       echo "$2...Success"
+         echo "$2...Success"
     fi
 }
 

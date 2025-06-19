8cb7e4fdc537fdafcfa4141395a343e6d6af4768
Naveen-Avula <avulanaveen437@gmail.com>
2025-06-19 19:46:07 +0530

    msg

--- 11.Functions.sh
+++ 11.Functions.sh
@@ -10,8 +10,14 @@ echo "you are the root user"
 fi
 
 VALIDATE(){
-    echo "Exit status:$1"
-    echo "what are u dng: $2"
+    if [$1 -ne 0]
+    then 
+    echo "$2...Failure"
+    exit 1
+    else
+    echo"$2...Success"
+    fi
+
 }
 
 

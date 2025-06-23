3e0bc5147783a9c3750a8c02219e89b3d8462f35
Naveen-Avula <avulanaveen437@gmail.com>
2025-06-23 12:08:03 +0530

    msg

--- 14.install-packages.sh
+++ 14.install-packages.sh
@@ -3,6 +3,10 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 if [ $USERID -ne 0 ]
 then
@@ -28,9 +32,9 @@ echo "package to install :$i"
 dnf list installed $i &>>$LOGFILE
 if [ $? -eq 0 ] 
 then 
-echo "$i is already installed..SKIPPING"
+echo -e "$i is already installed.. $Y SKIPPING $N"
 else
-echo "$i not installed need to ..INSTALL"
+echo -e "$i not installed ...need to INSTALL "
 
 fi
 done

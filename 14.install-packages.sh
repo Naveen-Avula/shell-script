ea78d368da38d0bb6aa611e17bb5b64c99393c73
Naveen-Avula <avulanaveen437@gmail.com>
2025-06-23 11:50:41 +0530

    msg

--- 14.install-packages.sh
+++ 14.install-packages.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 if [ $USERID -ne 0 ]
 then
@@ -9,4 +12,23 @@ else
     echo "You are super user."
 fi
 
-echo "All packages:$@"
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo "$2... FAILURE"
+        exit 1
+   else
+       echo "$2... SUCCESS"
+   fi
+}
+
+for i in $@
+do 
+echo "package to install :$i"
+dnf list installed $i &>>$LOGFILE
+if [$? -eq 0] 
+then 
+echo "$i is already installed..SKIPPING"
+
+fi
+done

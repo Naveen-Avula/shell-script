a5d0086f42816dfb01acd4f0bef09ad342d83bdf
Naveen-Avula <avulanaveen437@gmail.com>
2025-07-21 14:02:00 +0530

    msg

---
+++ 15.current-script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+COURSE= "Devops from current script"
+
+echo "Before calling the other script, couse:$COURSE"
+
+echo "process instance id of the current script: $$"
+
+#./16.other-script.sh
+
+echo "After calling other script, course: $COURSE"

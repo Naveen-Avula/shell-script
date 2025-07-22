9415756a6ecde9be88eb3cb5314ce7d83c5d0b11
Naveen-Avula <avulanaveen437@gmail.com>
2025-07-22 18:09:55 +0530

    msg

--- 18.delete-old-logs.sh
+++ 18.delete-old-logs.sh
@@ -2,3 +2,15 @@
 
 SOURCE_DIRECTORY=/tmp/app-logs
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [-d $SOURCE_DIRECTORY ]
+then
+    echo -e "$SOURCE_DIRECTORY directory exists...$G CONTINUING $N"
+else
+    echo -e "$SOURCE_DIRECTORY directory does not exist...$R EXITING $N"
+    exit 1
+fi

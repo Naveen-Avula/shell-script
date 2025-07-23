380e759ddd0b5f2a1b5942c862079be8ea84eec6
Naveen-Avula <avulanaveen437@gmail.com>
2025-07-23 14:11:01 +0530

    msg

--- 19.disk-usage.sh
+++ 19.disk-usage.sh
@@ -10,7 +10,7 @@ do
   
   if [ $USAGE -ge $DISK_THRESHOLD ]
   then
-   echo "$FOLDER is more than $DISK_USAGE, current usage is : $USAGE"
+   echo "$FOLDER is more than $DISK_USAGE, current usage is"
     
   fi
 done <<< $DISK_USAGE

171b4cc5d1edb58e8175e40adee621d155757418
Naveen-Avula <avulanaveen437@gmail.com>
2025-07-23 13:39:30 +0530

    msg

---
+++ 19.disk-usage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT |grep xfx)
+DISK_THRESHOLD=75
+
+while IFS=read -r line
+do
+USAGE=$(echo $line |awk -F " " '{print $6F}' | cut -d "%" -f1)
+FOLDER=$(echo $line |awk -F " " '{print $NF}')
+
+if [ $USAGE -ge $DISK_THRESHOLD ]
+then 
+echo "$FOLDER is more than $DISK_THRESHOLD% full, current usage is $USAGE%"
+fi
+
+done <<< ${DISK_USAGE}

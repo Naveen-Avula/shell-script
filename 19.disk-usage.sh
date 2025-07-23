dacfa0ea49e9b81a171e24df93d00badab6ba083
Naveen-Avula <avulanaveen437@gmail.com>
2025-07-23 13:49:13 +0530

    msg

--- 19.disk-usage.sh
+++ 19.disk-usage.sh
@@ -5,8 +5,8 @@ DISK_THRESHOLD=7
 
 while IFS= read -r line
 do
-  USAGE=$(echo $line |awk -F " " '{print $6F}' | cut -d "%" -f1)
-  FOLDER=$(echo $line |awk -F " " '{print $NF}')
+  USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+  FOLDER=$(echo $line | awk -F " " '{print $NF}')
 
 if [ $USAGE -ge $DISK_THRESHOLD ]
 then 
